b7a906d6ac94d880953af2f94bd7b2e6bb050c8d
TomKALBE <t.kalbe@h3hitema.fr>
2022-05-02 15:24:30 +0200

    ajout d un try catch pour gerer les erreurs 500

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const http = require('http');
 const port = 5000;
 
 const server = http.createServer((req, res) => {
-  if(req.method === 'GET'){
-    switch (req.url){
-      case "/":
-        res.write('<h1>HELLO WORLD TOM</h1>');
-        break;
-      default:
-        res.write('<h1>ERROR 404 PAGE NOT FOUND</h1>');
+  try{
+    if(req.method === 'GET'){
+      switch (req.url){
+        case "/":
+          res.write('<h1>HELLO WORLD TOM</h1>');
+          break;
+        default:
+          res.write('<h1>ERROR 404 PAGE NOT FOUND</h1>');
+      }
+    }else{
+      res.write('<h1>ERROR 405 METHOD NOT ALLOWED</h1>');
     }
-  }else{
-    res.write('<h1>ERROR 405 METHOD NOT ALLOWED</h1>');
+  }catch(e){
+    res.write(`<h1>ERROR 500 INTERAL SERVER ERROR ${e}</h1>`);
   }
   res.end();
+
+  
 });
 
 server.listen(port);
