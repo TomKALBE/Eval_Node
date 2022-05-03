3b7b79a2e388427983a8a7a47c4883592e04b91d
TomKALBE <t.kalbe@h3hitema.fr>
2022-05-03 10:22:47 +0200

    Creation d une route dynamique qui prends en compte le jpg png jpeg js css

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const fs = require("fs");
 const path = require('path');
+const { match } = require('assert');
 const port = 5000;
-
+const regex_ = /\public\/[A-Za-z0-9]+\/[A-Za-z0-9]+[.][A-Za-z0-9]+/;
 const returnPage = (res, code, file) => {
   res.writeHead(code, {
     'Content-Type': 'text/html'
@@ -12,37 +13,41 @@ const returnPage = (res, code, file) => {
   });
   res.write(data)
 }
+const image_type = {
+  'Content-Type': 'image/jpeg'
+}
+const javascript_type = {
+  'Content-Type': 'text/javascript'
+}
+const css_type = {
+  'Content-Type': 'text/css'
+}
+const CONTENT_TYPES = {
+  'jpg': image_type,
+  'jpeg': image_type,
+  'png': image_type,
+  'javascript':javascript_type,
+  'css':css_type
+}
 const server = http.createServer((req, res) => {
   let data =""
   try{
     if(req.method === 'GET'){
-      switch (req.url){
-        case "/":
+      if (req.url === "/"){
           returnPage(res, 200,'/public/pages/index.html' );       
-          break;
-        case "/public/images/image.jpg":
-          res.writeHead(200,{'Content-Type': 'image/jpeg'})
-          data = fs.readFileSync(path.resolve(__dirname+'/public/images/image.jpg'));
-          res.write(data);
-          break;
-        case "/public/css/style.css":
-          res.writeHead(200,{'Content-Type': 'text/css'})
-          data = fs.readFileSync(path.resolve(__dirname+'/public/css/style.css'));
-          res.write(data);
-          break;
-        case "/public/js/script.js":
-          res.writeHead(200,{'Content-Type': 'text/javascript'})
-          data = fs.readFileSync(path.resolve(__dirname+'/public/js/script.js'));
-          res.write(data);
-          break;
-        default:
-          console.log(req.url)
-          returnPage(res, 404,'/public/pages/erreur404.html' );
+      }
+      else if(regex_.test(req.url)){
+        res.writeHead(200,CONTENT_TYPES[req.url.split('.').pop()])
+        data = fs.readFileSync(path.resolve(__dirname+ req.url));
+        res.write(data);
+      }else{
+        returnPage(res, 404,'/public/pages/erreur404.html' );
       }
     }else{
       returnPage(res, 405,'/public/pages/erreur405.html' );
     }
   }catch(e){
+    console.log(e)
     returnPage(res, 500,'/public/pages/erreur500.html' );
   }
   res.end();
